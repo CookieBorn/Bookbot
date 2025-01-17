68a605b8e0c473bc584204b20ab9c89d66d211d9
CookieBorn <robanikin@hotmail.com>
2025-01-17 15:24:50 +1100

    C: word count

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from itertools import count
 def main():
     with open('books/frankenstein.txt') as f:
         file_contents = f.read()
     return file_contents
 file_contents=main()
-print(file_contents)
+def wordcount():
+    words=file_contents.split()
+    number=len(words)
+    return number
+words=wordcount()
+print(words)
