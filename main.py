e4e54d4fa8a5216bec06c721a57529be230f4d57
CookieBorn <robanikin@hotmail.com>
2025-01-17 21:27:04 +1100

    F: Report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def main():
     with open('books/frankenstein.txt') as f:
+        print(f"Begin report of {f.name}")
         file_contents = f.read()
     return file_contents
 file_contents=main()
@@ -7,9 +8,7 @@ def wordcount():
     words=(file_contents.casefold()).split()
     return words
 words=wordcount()
-print(words)
 number=len(words)
-print(number)
 letters={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
 alphabet={"a":0,"b":0,"c":0,"d":0,"e":0,"f":0,"g":0,"h":0,"i":0,"j":0,"k":0,"l":0,"m":0,"n":0,"o":0,"p":0,"q":0,"r":0,"s":0,"t":0,"u":0,"v":0,"w":0,"x":0,"y":0,"z":0}
 
@@ -19,12 +18,11 @@ def lettercount():
             alphabet[letter]=alphabet[letter]+word.count(letter)
     return alphabet
 letter_count=lettercount()
-print(letter_count)
-
+print(f"There is a total of {number} words")
 def report():
     for letter in letters:
         a=f"There are {letter_count[letter]} occurrences of {letter}"
         print(a)
     return
 reports=report()
-print(reports)
+print("End of Report")
